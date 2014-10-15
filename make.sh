3117d73a5aa68132a808ed5f1080d86c9892f875
lüftreich <luftreich@gmail.com>
2014-10-15 16:14:38 +0800

    update

--- make.sh
+++ make.sh
@@ -1,1 +1,8 @@
-python addons_xml_and_zipfile_generator.py 
+echo
+cat src/repository.imaxpo/addon.xml | head -n5
+echo '....'
+echo -n '<!> 确认repo已经更新 ?? [Y/n] '
+read LUFT
+[ "$LUFT" = "Y" -o "$LUFT" = "y" ] && {
+    python addons_xml_and_zipfile_generator.py 
+}
